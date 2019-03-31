47c4f44c6df685e5eb450f9e1ac885d1d0cb42c4
bksun <sunilsinghmbm@gmail.com>
2019-03-31 17:38:00 +0530

    Add list in the component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {  
-      counter: 3    
-  }
+  state = {
+    counter: 3,
+    tags: ["tag1", "tag2", "tag3"]
+  };
 
   render() {
     return (
       <div>
-        <span className = {this.getBadgeClasse()} >{this.codeFormatter()}</span>
-        <button className="btn btn-secondary btn-sm" >Counter</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
-  }
-
-    getBadgeClasse() {
-        let classes = "badge m-2 badge-";
-        return classes += this.state.counter === 0 ? "warning" : "primary";
-    }
-
-    codeFormatter() {
-      return this.state.counter === 0 ? 'Zero' : this.state.counter;
   }
 }
 
